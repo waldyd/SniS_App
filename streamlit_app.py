7d005ef6b418e7bf68218233bf0d1d8635786f5f
Nestor Waldyd <nwaldyd@yahoo.co.uk>
2024-08-10 21:00:58 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
 
 def clear_session():
     st.session_state["text"] = ""
