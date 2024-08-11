2d00643acbae764aeb58ff2c80fd253583229921
Nestor Waldyd <nwaldyd@yahoo.co.uk>
2024-08-10 21:04:32 -0500

    API Integration JSON response

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,4 +76,4 @@ if ingredients_list:
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon", verify=False)
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
